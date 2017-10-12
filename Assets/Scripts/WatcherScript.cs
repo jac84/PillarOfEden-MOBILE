4d7b5b234adbca4a6835c43cdb5644698418b3c1
Joseph Kullmann <jwk9@njit.edu>
2017-10-12 15:35:55 -0400

    initial commit

---
+++ Assets/Scripts/WatcherScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WatcherScript : MonoBehaviour {
+    [SerializeField]
+    private GameObject[] myTowers;
+
+	// Update is called once per frame
+	void Update () {
+        if(Input.touchCount > 0)
+        {
+            Touch myTouch = Input.touches[0]; // The first touch
+            Instantiate(myTowers[0], new Vector3(myTouch.position.x, myTouch.position.y, 0), Quaternion.identity);
+            // Just instantiates a rook tower where the touch was
+        }
+	}
+}
