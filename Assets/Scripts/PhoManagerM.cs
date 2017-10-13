5e9e451ddb533a9718c90195b16e99b96d883d22
Hitsugata <zakie95hinds@gmail.com>
2017-10-13 19:58:01 -0400

    Quick connect script

---
+++ Assets/Scripts/PhoManagerM.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PhoManagerM : Photon.MonoBehaviour
+{
+
+    void Start()
+    {
+        
+        PhotonNetwork.ConnectUsingSettings("test");
+        //Debug.Log(PhotonNetwork.connectionStateDetailed.ToString());
+    }
+
+    public virtual void OnJoinedLobby()
+    {
+        RoomOptions roomops = new RoomOptions() { isVisible = true, MaxPlayers = 2 };
+        PhotonNetwork.JoinOrCreateRoom("hello", roomops, TypedLobby.Default);
+
+    }
+
+
+}
