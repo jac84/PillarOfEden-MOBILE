233611ef4b94ca5899f592021a89b0af877c9685
Hitsugata <zakie95hinds@gmail.com>
2017-10-31 00:02:51 -0400

    Tower Select

---
+++ Assets/Scripts/TowerSelect.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class TowerSelect : MonoBehaviour {
+    [SerializeField] private string[] Towers;
+    [SerializeField] private int currentTower;
+    [SerializeField] private Text ButtonTower;
+	// Use this for initialization
+    void Start()
+    {
+        ButtonTower.text = Towers[currentTower];
+    }
+
+    public void TowerSwitch()
+    {
+        for(int i = 0; i < Towers.Length; i++)
+        {
+            if (currentTower == i)
+            {
+                if ((currentTower + 2) > Towers.Length)
+                {
+                    currentTower = 0;
+                    ButtonTower.text = Towers[currentTower];
+                    return;
+                }
+                else
+                {
+                    currentTower++;
+                    ButtonTower.text = Towers[currentTower];
+                    return;
+                }
+            }
+        }
+    }
+    public string CurrentTower()
+    {
+        return Towers[currentTower];
+    }
+}
--- Assets/Scripts/WatcherScript.cs
+++ Assets/Scripts/WatcherScript.cs
@@ -4,14 +4,14 @@ using System.Collections;
 public class WatcherScript : Photon.MonoBehaviour {
     [SerializeField]
     private string[] myTowers;
-
+    [SerializeField] TowerSelect towerselect;
 	// Update is called once per frame
 	void Update () {
         if(Input.touchCount > 0 && Input.GetTouch(0).phase==TouchPhase.Began)
         {
             Vector3 TouchPos = Input.GetTouch(0).position;
             TouchPos.z = TouchPos.y;
-            PhotonNetwork.Instantiate(myTowers[0], Camera.main.ScreenToWorldPoint(TouchPos), Quaternion.identity,0);
+            PhotonNetwork.Instantiate(towerselect.CurrentTower(), Camera.main.ScreenToWorldPoint(TouchPos), Quaternion.identity,0);
             // Just instantiates a rook tower where the touch was
         }
 	}
