76dd6891868adfd925483baf87843930942cc91e
Hitsugata <zakie95hinds@gmail.com>
2017-11-02 00:10:27 -0400

    Midterm is go

--- Assets/Scripts/offsetmobile.cs
+++ Assets/Scripts/offsetmobile.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class offsetmobile : MonoBehaviour {
     public float offsetxrotation;
+    public float offsety;
 	// Use this for initialization
 	void Start () {
         
@@ -12,5 +13,6 @@ public class offsetmobile : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         transform.rotation = Quaternion.Euler(offsetxrotation, transform.rotation.y, transform.rotation.z);
+        transform.position = new Vector3(transform.position.x, 0, transform.position.z);
     }
 }
